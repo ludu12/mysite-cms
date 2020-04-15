2aaae2c89dc7b258b63f8ebfc27f6406c64264e1
Luke Dunscombe <dunscombe1@gmail.com>
2020-04-15 18:18:06 -0500

    Adding tool to tech stack

--- src/lists/tech-stack.js
+++ src/lists/tech-stack.js
@@ -5,6 +5,6 @@ export const TechStackSchema = {
   labelField: 'name',
   fields: {
     name: { type: Text, isRequired: true, isUnique: true },
-    type: { type: Select, isRequired: true, options: 'language, framework, platform, hosting' }
+    type: { type: Select, isRequired: true, options: 'language, framework, platform, hosting, tool' }
   },
 };
